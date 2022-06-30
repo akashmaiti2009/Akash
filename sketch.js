f99b93681cfb21df86777838c0aed56472042fb4
akashmaiti2009 <105442636+akashmaiti2009@users.noreply.github.com>
2022-06-30 20:19:05 +0530

    Add files via upload
    project

--- sketch.js
+++ sketch.js
@@ -1,191 +1,157 @@
-var path, mainCyclist;
-var player1, player2, player3;
-var pathImg, mainRacerImg1, mainRacerImg2;
+var path, boy, cash, diamonds, jwellery, sword;
+var pathImg, boyImg, cashImg, diamondsImg, jwelleryImg, swordImg;
+var treasureCollection = 0;
+var cashG, diamondsG, jwelleryG, swordGroup;
 
-var oppPink1Img, oppPink2Img;
-var oppYellow1Img, oppYellow2Img;
-var oppRed1Img, oppRed2Img;
-var gameOverImg, cycleBell;
-
-var pinkCG, yellowCG, redCG;
-
-var END = 0;
+//Game States
 var PLAY = 1;
-var gameState = PLAY;
-
-var distance = 0;
-var gameOver, restart;
+var END = 0;
+var gameState = 1;
 
 function preload() {
   pathImg = loadImage("Road.png");
-  mainRacerImg1 = loadAnimation("mainPlayer1.png", "mainPlayer2.png");
-  mainRacerImg2 = loadAnimation("mainPlayer3.png");
-
-  oppPink1Img = loadAnimation("opponent1.png", "opponent2.png");
-  oppPink2Img = loadAnimation("opponent3.png");
-
-  oppYellow1Img = loadAnimation("opponent4.png", "opponent5.png");
-  oppYellow2Img = loadAnimation("opponent6.png");
-
-  oppRed1Img = loadAnimation("opponent7.png", "opponent8.png");
-  oppRed2Img = loadAnimation("opponent9.png");
-
-  cycleBell = loadSound("bell.mp3");
-  gameOverImg = loadImage("gameOver.png");
+  boyImg = loadAnimation("Runner-1.png", "Runner-2.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jwelleryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  endImg = loadAnimation("gameOver.png");
 }
 
 function setup() {
 
-  createCanvas(1200, 300);
-  // Moving background
-  path = createSprite(100, 150);
+  //create the canvas and adjust the window sizes to suit the device 
+
+  createCanvas(windowWidth, windowHeight);
+
+  path = createSprite(width / 2, height / 2);
   path.addImage(pathImg);
-  path.velocityX = -5;
+  path.velocityY = 4
+  path.y = height / 2;
+
+
 
   //creating boy running
-  mainCyclist = createSprite(70, 150);
-  mainCyclist.addAnimation("SahilRunning", mainRacerImg1);
-  mainCyclist.scale = 0.07;
-
-  //set collider for mainCyclist
+  boy = createSprite(width / 2, height - 20, 20, 20);
+  boy.addAnimation("SahilRunning", boyImg);
+  boy.scale = 0.08;
 
 
-  mainCyclist.setCollider("rectangle", 0, 0, 40, 40);
-
-
-
-  gameOver = createSprite(650, 150);
-  gameOver.addImage(gameOverImg);
-  gameOver.scale = 0.8;
-  gameOver.visible = false;
-
-  pinkCG = new Group();
-  yellowCG = new Group();
-  redCG = new Group();
+  cashG = new Group();
+  diamondsG = new Group();
+  jwelleryG = new Group();
+  swordGroup = new Group();
 
 }
 
 function draw() {
-  background(0);
-
-  drawSprites();
-  textSize(20);
-  fill(255);
-  text("Distance: " + distance, 900, 30);
 
   if (gameState === PLAY) {
-
-    distance = distance + Math.round(getFrameRate() / 50);
-    path.velocityX = -(6 + 2 * distance / 150);
-
-    mainCyclist.y = World.mouseY;
+    background(0);
+    boy.x = World.mouseX;
 
     edges = createEdgeSprites();
-    mainCyclist.collide(edges);
+    boy.collide(edges);
 
     //code to reset the background
-    if (path.x < 0) {
-      path.x = width / 2;
+    if (path.y > 400) {
+      path.y = height / 2;
     }
 
-    //code to play cycle bell sound
-    if (keyDown("space")) {
-      cycleBell.play();
+    createCash();
+    createDiamonds();
+    createJwellery();
+    createSword();
+
+    if (cashG.isTouching(boy)) {
+      cashG.destroyEach();
+      treasureCollection = treasureCollection + 50;
     }
+    else if (diamondsG.isTouching(boy)) {
+      diamondsG.destroyEach();
+      treasureCollection = treasureCollection + 100;
 
-    //creating continous opponent players
-    var select_oppPlayer = Math.round(random(1, 3));
+    } else if (jwelleryG.isTouching(boy)) {
+      jwelleryG.destroyEach();
+      treasureCollection = treasureCollection + 150;
 
-    if (World.frameCount % 150 == 0) {
-      if (select_oppPlayer == 1) {
-        pinkCyclists();
-      } else if (select_oppPlayer == 2) {
-        yellowCyclists();
-      } else {
-        redCyclists();
+    } else {
+      if (swordGroup.isTouching(boy)) {
+        gameState = END;
+
+        boy.addAnimation("SahilRunning", endImg);
+        boy.x = width / 2;
+        boy.y = height / 2;
+        boy.scale = 0.6;
+
+        cashG.destroyEach();
+        diamondsG.destroyEach();
+        jwelleryG.destroyEach();
+        swordGroup.destroyEach();
+
+        cashG.setVelocityYEach(0);
+        diamondsG.setVelocityYEach(0);
+        jwelleryG.setVelocityYEach(0);
+        swordGroup.setVelocityYEach(0);
+
       }
     }
 
-    if (pinkCG.isTouching(mainCyclist)) {
-      gameState = END;
-      player1.velocityY = 0;
-      player1.addAnimation("opponentPlayer1", oppPink2Img);
-    }
-
-    if (yellowCG.isTouching(mainCyclist)) {
-      gameState = END;
-      player2.velocityY = 0;
-      player2.addAnimation("opponentPlayer2", oppYellow2Img);
-    }
-
-    if (redCG.isTouching(mainCyclist)) {
-      gameState = END;
-      player3.velocityY = 0;
-      player3.addAnimation("opponentPlayer3", oppRed2Img);
-    }
-
-  } else if (gameState === END) {
-    gameOver.visible = true;
-
+    drawSprites();
     textSize(20);
     fill(255);
-    text("Press Up Arrow to Restart the game!", 500, 200);
+    text("Treasure: " + treasureCollection, width - 150, 30);
+  }
 
-    path.velocityX = 0;
-    mainCyclist.velocityY = 0;
-    mainCyclist.addAnimation("SahilRunning", mainRacerImg2);
+}
 
-    pinkCG.setVelocityXEach(0);
-    pinkCG.setLifetimeEach(-1);
-
-    yellowCG.setVelocityXEach(0);
-    yellowCG.setLifetimeEach(-1);
-
-    redCG.setVelocityXEach(0);
-    redCG.setLifetimeEach(-1);
-
-
-    if (keyDown("UP_ARROW")) {
-      reset();
-    }
+function createCash() {
+  if (World.frameCount % 200 == 0) {
+    // Modify the positions of cash 
+    var cash = createSprite(Math.round(random(20, windowWidth - 20), 40, 10, 10));
+    cash.addImage(cashImg);
+    cash.scale = 0.12;
+    cash.velocityY = 5;
+    cash.lifetime = 200;
+    cashG.add(cash);
   }
 }
 
-function pinkCyclists() {
-  player1 = createSprite(1100, Math.round(random(50, 250)));
-  player1.scale = 0.06;
-  player1.velocityX = -(6 + 2 * distance / 150);
-  player1.addAnimation("opponentPlayer1", oppPink1Img);
-  player1.setLifetime = 170;
-  pinkCG.add(player1);
+function createDiamonds() {
+  if (World.frameCount % 320 == 0) {
+    // Modify the positions of diamonds 
+
+    var diamonds = createSprite(Math.round(random(20, windowWidth - 20), 40, 10, 10));
+    diamonds.addImage(diamondsImg);
+    diamonds.scale = 0.03;
+    diamonds.velocityY = 5;
+    diamonds.lifetime = 200;
+    diamondsG.add(diamonds);
+  }
 }
 
-function yellowCyclists() {
-  player2 = createSprite(1100, Math.round(random(50, 250)));
-  player2.scale = 0.06;
-  player2.velocityX = -(6 + 2 * distance / 150);
-  player2.addAnimation("opponentPlayer2", oppYellow1Img);
-  player2.setLifetime = 170;
-  yellowCG.add(player2);
+function createJwellery() {
+  if (World.frameCount % 410 == 0) {
+    //   Modify the positions of jwellery to make them spawn throughout the available screen size.
+
+    var jwellery = createSprite(Math.round(random(20, windowWidth - 20), 40, 10, 10));
+    jwellery.addImage(jwelleryImg);
+    jwellery.scale = 0.13;
+    jwellery.velocityY = 5;
+    jwellery.lifetime = 200;
+    jwelleryG.add(jwellery);
+  }
 }
 
-function redCyclists() {
-  player3 = createSprite(1100, Math.round(random(50, 250)));
-  player3.scale = 0.06;
-  player3.velocityX = -(6 + 2 * distance / 150);
-  player3.addAnimation("opponentPlayer3", oppRed1Img);
-  player3.setLifetime = 170;
-  redCG.add(player3);
-}
+function createSword() {
+  if (World.frameCount % 530 == 0) {
+    //   Modify the positions of sword to make them spawn throughout the available screen size.
 
-function reset() {
-  gameState = PLAY;
-  gameOver.visible = false;
-  mainCyclist.addAnimation("SahilRunning", mainRacerImg1);
-
-  pinkCG.destroyEach();
-  yellowCG.destroyEach();
-  redCG.destroyEach();
-
-  distance = 0;
+    var sword = createSprite(Math.round(random(20, windowWidth - 20), 40, 10, 10));
+    sword.addImage(swordImg);
+    sword.scale = 0.1;
+    sword.velocityY = 4;
+    sword.lifetime = 200;
+    swordGroup.add(sword);
+  }
 }
