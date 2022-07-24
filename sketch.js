9616ce0ba891e156a483a94349f0a30bf917b095
akashmaiti2009 <105442636+akashmaiti2009@users.noreply.github.com>
2022-07-24 11:46:59 +0530

    Add files via upload
    project

--- sketch.js
+++ sketch.js
@@ -1,84 +1,53 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var ball, groundObj, leftSide, rightSide;
-var world;
-var dustbinImg, paperImg;
-var radius = 70;
-
-function preload() {
-	//find the bug in the below code
-	dustbinImg = loadImage("dustbin.png");
-
-
-	paperImg = loadImage("paper.png");
-
-
-	//keyPressed(UP_ARROW)
-	//fill(225)
-
-}
-
+let engine;
+let world;
+var ball;
+var ground;
+var wedge;
+var angle=60;
+var poly;
+var boxes=[];
 
 function setup() {
-	createCanvas(1600, 700);
-	rectMode(CENTER);
+  createCanvas(400,400);
 
-	engine = Engine.create();
-	world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
+  var option={
+    isStatic:true
+  };
+  ground=Bodies.rectangle(100,300,400,20,option);
+  World.add(world,ground);
+  //box1 = new Box(200,100,50,50);
+  
 
-
-	var ball_options = {
-		isStatic: false,
-		restitution: 0.3,
-		density: 0.4
-	}
-
-	ball = Bodies.circle(260, 100, radius / 2.6, ball_options);
-	World.add(world, ball);
-
-	ground = new Ground(width / 2, 670, width, 20);
-	leftSide = new Ground(1100, 600, 10, 120);
-	rightSide = new Ground(1270, 600, 10, 120);
-	bottomSide = new Ground(1185, 650, 150, 20);
-
-	Engine.run(engine);
-
-
-
-
-
-
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+function mousePressed(){
+  boxes.push(
+    
+    
+    new Box(mouseX,mouseY,50,50)
+    
+    
+    
+    )
 }
 
-
-function draw() {
-	background(200);
-	rectMode(CENTER);
-
-
-	ground.display();
-	leftSide.display();
-	rightSide.display();
-	bottomSide.display();
-
-
-	//imageMode(CENTER);
-	//use image() command to add paper image to the ball
-	image(paperImg, ball.position.x, ball.position.y, radius, radius);
-
-	// use image() command to add dustbin image in the canvas.
-	image(dustbinImg, 1089, 470, 200, 200);
-
-
+function draw() 
+{
+  background(51);
+  rect(ground.position.x,ground.position.y,400,10);
+  Engine.update(engine);
+ for(var i=0;i<boxes.length;i++) {
+ boxes[i].show();}
+  
+  
 }
 
-function keyPressed() {
-	if (keyCode === UP_ARROW) {
-
-		Matter.Body.applyForce(ball, ball.position, { x: 85, y: -85 });
-
-	}
-}
